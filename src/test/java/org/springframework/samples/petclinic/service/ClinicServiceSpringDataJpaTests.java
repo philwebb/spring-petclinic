4859334023b8bcaeedcaf0e8c68390be0ab29da0
Phillip Webb <pwebb@pivotal.io>
2014-09-19 14:10:18 -0700

    Whitespace

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceSpringDataJpaTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceSpringDataJpaTests.java
@@ -9,11 +9,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * <p> Integration test using the 'Spring Data' profile. 
+ * <p> Integration test using the 'Spring Data' profile.
  * @see AbstractClinicServiceTests AbstractClinicServiceTests for more details. </p>
  * @author Michael Isvy
  */
-
 @SpringApplicationConfiguration(classes = PetClinicApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("spring-data-jpa")
