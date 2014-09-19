9266ff3099bbb0607d574691cc6a43ef074350a6
Phillip Webb <pwebb@pivotal.io>
2014-09-19 14:34:59 -0700

    Drop bean output from main

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,13 +16,10 @@
 
 package org.springframework.samples.petclinic;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,17 +36,6 @@ public class PetClinicApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ctx = SpringApplication.run(PetClinicApplication.class, args);
-
-		System.out.println("Active Profiles: "
-				+ Arrays.toString(ctx.getEnvironment().getActiveProfiles()));
-
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
-
+		SpringApplication.run(PetClinicApplication.class, args);
 	}
 }
