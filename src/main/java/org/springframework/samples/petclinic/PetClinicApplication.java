c068b9efd356c6426c474ff876061da9489e839d
Phillip Webb <pwebb@pivotal.io>
2014-09-19 14:42:17 -0700

    Switch to JAR

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,22 +18,21 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * PetClinic Spring Boot Application.
+ *
+ * @author Fabien Lauf
+ */
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class PetClinicApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PetClinicApplication.class);
-	}
+public class PetClinicApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
+
 }
